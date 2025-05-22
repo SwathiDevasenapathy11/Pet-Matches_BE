527a732386cd962257fb09ac280fd8140d5d20e3
SwathiDevasenapathy <swathid@datapattern.ai>
2025-05-22 19:42:55 +0530

    Initial push

---
+++ src/test/java/com/example/petmatch/PetmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
